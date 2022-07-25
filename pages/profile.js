7b74620b37e38da687645be1655192af3eefc432
briiiqtt <briiiqtt@gmail.com>
2022-07-25 23:00:40 +0900

    페이지 구현 완료

--- pages/profile.js
+++ pages/profile.js
@@ -1,4 +1,32 @@
+import Head from "next/head";
+
+import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+
 const Profile = () => {
-  return <div>내 프로필</div>;
+  const followerList = [
+    { nickname: "ㅎㅇ" },
+    { nickname: "ㅎㅇㅎㅇ" },
+    { nickname: "ㅎㅇㅎㅇㅎㅇ" },
+  ];
+  const followingList = [
+    { nickname: "ㅂㅇ" },
+    { nickname: "ㅂㅇㅂㅇ" },
+    { nickname: "ㅂㅇㅂㅇㅂㅇ" },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
+    </>
+  );
 };
 export default Profile;
--- pages/signup.js
+++ pages/signup.js
@@ -1,4 +1,115 @@
+import Head from "next/head";
+import { useState, useCallback, useMemo } from "react";
+
+import { Form, Input, Checkbox, Button } from "antd";
+import AppLayout from "../components/AppLayout";
+import useInput from "../hooks/useInput";
+
 const Signup = () => {
-  return <div>회원가입</div>;
+  const [id, onChangeId] = useInput("");
+
+  const [nickname, onChangeNickname] = useInput("");
+
+  const [password, onChangePassword] = useInput("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const onChanngePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
+
+  const [term, setTerm] = useState("");
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
+  }, []);
+
+  const formStyle = useMemo(() => {
+    return { padding: "10px" };
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+    console.log(id, nickname, password);
+  }, [password, passwordCheck, term]);
+
+  const errorStyle = useMemo(() => {
+    return { color: "red" };
+  }, []);
+
+  return (
+    <>
+      <AppLayout>
+        <Head>
+          <title>회원가입 | NodeBird</title>
+        </Head>
+        <Form onFinish={onSubmit}>
+          <div>
+            <label htmlFor="user-id">아이디</label>
+            <br />
+            <Input name="user-id" value={id} required onChange={onChangeId} />
+          </div>
+          <div>
+            <label htmlFor="user-nickname">닉네임</label>
+            <br />
+            <Input
+              name="user-nickname"
+              value={nickname}
+              required
+              onChange={onChangeNickname}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password">비밀번호</label>
+            <br />
+            <Input
+              name="user-password"
+              value={password}
+              required
+              type="password"
+              onChange={onChangePassword}
+            />
+            {passwordError && (
+              <div style={errorStyle}>비밀번호가 일치하지 않습니다</div>
+            )}
+          </div>
+          <div>
+            <label htmlFor="user-password-check">비밀번호확인</label>
+            <br />
+            <Input
+              name="user-password-check"
+              value={passwordCheck}
+              required
+              type="password"
+              onChange={onChanngePasswordCheck}
+            />
+            {passwordError && (
+              <div style={errorStyle}>비밀번호가 일치하지 않습니다</div>
+            )}
+          </div>
+          <div>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              이용약관에 동의합니다
+            </Checkbox>
+            {termError && <div style={{ errorStyle }}>약관에 동의하세욥</div>}
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <Button type="primary" htmlType="submit">
+              가입하기
+            </Button>
+          </div>
+        </Form>
+      </AppLayout>
+    </>
+  );
 };
 export default Signup;
