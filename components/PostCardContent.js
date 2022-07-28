46eaa00541fdde71284187e1336f8ee288e4a3bc
briiiqtt <briiiqtt@gmail.com>
2022-07-28 09:13:26 +0900

    a

--- components/PostCardContent.js
+++ components/PostCardContent.js
@@ -1,11 +1,11 @@
-import { Link } from "next/link";
+import Link from "next/link";
 
 const PostCardContent = ({ postData }) => (
   <div>
-    {postData.split(/(#[^\s#]+)/g).map((v) => {
+    {postData.split(/(#[^\s#]+)/g).map((v, i) => {
       if (v.match(/(#[^\s#]+)/)) {
         return (
-          <Link href={`/hashtag/${v.slice(1)}`}>
+          <Link href={`/hashtag/${v.slice(1)}`} key={i}>
             <a>{v}</a>
           </Link>
         );
