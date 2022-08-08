4fb4a67c7742b43bad5153bc8d283f543d2c7ec6
briiiqtt <briiiqtt@gmail.com>
2022-08-08 17:59:00 +0900

    a

--- back/app.js
+++ back/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+const db = require("./models");
+
+db.sequelize
+  .sync()
+  .then(() => console.log("db연결성공"))
+  .catch((e) => console.error(e));
+
 const postRouter = require("./routes/post");
 
 app.get("/", (req, res) => {
--- back/models/user.js
+++ back/models/user.js
@@ -3,18 +3,36 @@ const { sequelize } = require(".");
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
-    "User" /* User (모델이름) -> users(소문자, 단수형) */,
+    "User" /* User (모델이름) -> users(소문자, 복수형) */,
     {
       //id : id는 기본적으로 들어있다.
-      email: {},
-      nickname: {},
-      password: {},
+      email: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        unique: true,
+      },
+      nickname: { type: DataTypes.STRING(30), allowNull: false },
+      password: { type: DataTypes.STRING(100), allowNull: false },
     },
     {
       charset: "utf8",
       collate: "utf8_general_ci",
     }
   );
-  User.associate = (db) => {};
+  User.associate = (db) => {
+    db.User.hasMany(db.Post);
+    db.User.hasMany(db.Comment);
+    db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" }); //사용자와 게시글간의 좋아요관계
+    db.User.belongsToMany(db.User, {
+      through: "Follow",
+      as: "Followers",
+      foreignKey: "FollowingId",
+    });
+    db.User.belongsToMany(db.User, {
+      through: "Follow",
+      as: "Followings",
+      foreignKey: "FollowerId",
+    });
+  };
   return User;
 };
